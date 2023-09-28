4fb6c896f230ef4a92a2412baed10d7940bc6844
Nag <nagcloudlab@gmail.com>
2023-09-28 21:13:59 +0530

    directives

--- 02-play-with-angular/projects/shop-it/src/app/app.component.ts
+++ 02-play-with-angular/projects/shop-it/src/app/app.component.ts
@@ -6,14 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'shop-IT';
-  product: any = {
-    id: 1,
-    name: 'Laptop',
-    price: 1000000,
-    description: 'Laptop Description',
-    image: 'assets/Laptop.png'
+
+  products: Array<any> = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 1000000,
+      description: 'Laptop Description',
+      image: 'assets/Laptop.png',
+      isAvailable: true
+    },
+    // {
+    //   id: 2,
+    //   name: 'Mobile',
+    //   price: 1000,
+    //   description: 'Mobile Description',
+    //   image: 'assets/Mobile.png',
+    //   isAvailable: true
+    // }
+  ]
+
+  currentTab: number = 1
+
+  handleTabChange(index: number) {
+    this.currentTab = index
   }
+
+  isTabSelected(index: number): boolean {
+    return this.currentTab === index
+  }
+
   handleBuy(event: MouseEvent) {
     console.log(event);
   }
