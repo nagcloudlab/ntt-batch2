4870ca4b343b735f336e1694027b6a3585b9e7c0
Nag <nagcloudlab@gmail.com>
2023-09-29 19:15:25 +0530

    Thinking In Angular

--- 02-play-with-angular/projects/thinking-in-angular/src/app/voting-box/voting-box.component.ts
+++ 02-play-with-angular/projects/thinking-in-angular/src/app/voting-box/voting-box.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class VotingBoxComponent {
 
+
+  votingItems: Array<string> = [
+    "React.js",
+    "Angular.js",
+    "Vue.js",
+    "React Native",
+  ]
+
+  votingLines: Array<any> = [
+  ]
+
+  handleLikeVote(vote:any): void {
+    let {item}=vote
+    let votingLine = this.votingLines.find(line=>line.item===item)
+    if(votingLine) {
+      votingLine.likes++
+    }else{
+      this.votingLines.push({
+        id: this.votingLines.length+1,
+        item: item,
+        likes: 1,
+        dislikes: 0
+      })
+    }
+  }
+  handleDislikeVote(vote:any): void {
+    let {item}=vote
+    let votingLine = this.votingLines.find(line=>line.item===item)
+    if(votingLine) {
+      votingLine.dislikes++
+    }else{
+      this.votingLines.push({
+        id: this.votingLines.length+1,
+        item: item,
+        likes: 0,
+        dislikes: 1
+      })
+    }
+  }
+
 }
