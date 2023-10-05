e9f0ca6f32600a7953b12445fa8ad060d6e2259a
Nagabhushanam <nagabhushanam.n@revature.com>
2023-10-05 22:19:28 +0530

    Node: core concepts

--- 03-play-with-node/2-node-http-server/server-v6.js
+++ 03-play-with-node/2-node-http-server/server-v6.js
@@ -10,7 +10,6 @@ if (cluster.isPrimary) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
   cluster.on("exit", (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
     cluster.fork();
